5b5404004e2630b9eb587d80e44998603855ec7a
ipwebcamandrea-gif <ipwebcamandrea@gmail.com>
2026-06-11 09:37:25 +0200

    Update watchlist.py

--- pages/watchlist.py
+++ pages/watchlist.py
@@ -2,20 +2,13 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 from pathlib import Path
-
+from utils.auth import require_login
 
 # =========================
 # PROTEZIONE LOGIN
 # =========================
 
-if not st.session_state.get("authenticated", False):
-    st.error("Accesso non autorizzato.")
-
-    if st.button("Torna al Login"):
-        st.switch_page("main.py")
-
-    st.stop()
-
+require_login()
 
 # =========================
 # CONFIGURAZIONE FILE / CSS
