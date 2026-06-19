668e557058ec6868058a67df7ffbe10e004557b2
ipwebcamandrea-gif <ipwebcamandrea@gmail.com>
2026-06-19 13:05:42 +0200

    Update allocazione_portafoglio.py

--- pages/allocazione/allocazione_portafoglio.py
+++ pages/allocazione/allocazione_portafoglio.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from zoneinfo import ZoneInfo
 
 import streamlit as st
-from utils.app_branding import get_app_icon
+from utils.app_branding import apply_mobile_app_branding, get_app_icon
 
 from components.standard_header import render_standard_page_header
 
@@ -38,6 +38,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
 )
+apply_mobile_app_branding()
 
 
 def load_css() -> None:
